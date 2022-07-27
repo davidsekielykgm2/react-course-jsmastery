2465f9438ed0e9909edfb519b4c49dc964921829
davidsekielyk <david.sekielyk@gm2dev.com>
2022-07-27 15:30:09 -0300

    function searchMovies with input bar

--- src/App.js
+++ src/App.js
@@ -7,37 +7,61 @@ import searchMovies from './services/getSearchMovies'
 
 const App = () => {
   const [movies, setMovies] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  const [loading, setLoading] = useState(false)
 
   useEffect(() => {
-    searchMovies().then(data => {
-      setMovies([...data.results])
-    })
+    handleSetMovies()
   }, [])
+
+  const handleSetMovies = ({ title = 'Endgame' } = {}) => {
+    setLoading(true)
+    searchMovies({ title }).then(data => {
+      setMovies([...data.results])
+      setLoading(false)
+    })
+  }
 
   return (
     <div className="app">
       <h1>MovieLand</h1>
 
       <div className="search">
-        <input placeholder="Search for movies" />
-        <img src={SearchIcon} alt="search" />
+        <input
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search for movies"
+        />
+        <img
+          src={SearchIcon}
+          alt="search"
+          onClick={() => handleSetMovies({ title:searchTerm })}
+        />
       </div>
 
       <div className="container">
         {
-          movies?.length > 0
+          loading
           ? (
-            movies.map(data => (
-              <MovieCard
-                key={data.imdbid}
-                movie={data}
-              />
-            ))
+            <div className="empty">
+              <h2>Loading...</h2>
+            </div>
           )
           : (
-            <div className="empty">
-              <h2>No movies found</h2>
-            </div>
+            movies?.length > 0
+            ? (
+              movies.map(data => (
+                <MovieCard
+                  key={data.imdbid}
+                  movie={data}
+                />
+              ))
+            )
+            : (
+              <div className="empty">
+                <h2>No movies found</h2>
+              </div>
+            )
           )
         }
       </div>
